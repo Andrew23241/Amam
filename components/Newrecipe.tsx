4317f26e5d9fac0e953b671ebceebac499f49196
Andrew23241 <a110550056.cs10@nycu.edu.tw>
2024-11-09 12:07:34 +0800

    Update Newrecipe.tsx
    change title

--- components/Newrecipe.tsx
+++ components/Newrecipe.tsx
@@ -34,7 +34,7 @@ export default async function NewestR() {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            New Recipies
+            食譜
           </h2>
 
           <Link
